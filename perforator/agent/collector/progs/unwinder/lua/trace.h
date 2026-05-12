bfe465a99e3977835be020798a1b5655257c9a65
Vsevolod Kalshchikov <12762267+VsKlsch@users.noreply.github.com>
2026-05-12 15:02:15 +0300

    Temporary fix bad address error when running without --debug flag by changing BPF_TRACE to BPF_PRINTK for LUA_TRACE

--- perforator/agent/collector/progs/unwinder/lua/trace.h
+++ perforator/agent/collector/progs/unwinder/lua/trace.h
@@ -1,4 +1,5 @@
 #pragma once
 
+// Temporary change to get rid of bad address error when running without --debug flag
 #define LUA_TRACE(fmt, ...) \
-    BPF_TRACE("lua: " fmt "\n", ##__VA_ARGS__)
+    BPF_PRINTK("lua: " fmt "\n", ##__VA_ARGS__)
