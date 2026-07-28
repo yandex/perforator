7cbbc5e1b5de0c31805dd7f28cd4d598059e2552
Spar <developspartv@gmail.com>
2026-07-28 17:53:10 +0300

    Remove and move headers from `decode.h`

--- perforator/lib/lua/asm/x86/decode.h
+++ perforator/lib/lua/asm/x86/decode.h
@@ -1,50 +1,19 @@
 #pragma once
 
-#ifdef __GNUC__
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#endif
-#include <contrib/libs/llvm18/lib/Target/X86/X86InstrInfo.h>
-#ifdef __GNUC__
-#pragma GCC diagnostic pop
-#endif
+#include <llvm/TargetParser/Triple.h>
 
-#include <llvm/MC/MCAsmInfo.h>
-#include <llvm/MC/MCContext.h>
-#include <llvm/MC/MCDisassembler/MCDisassembler.h>
-#include <llvm/MC/MCInst.h>
-#include <llvm/MC/MCInstBuilder.h>
-#include <llvm/MC/MCObjectFileInfo.h>
-#include <llvm/MC/MCRegisterInfo.h>
-#include <llvm/MC/MCSubtargetInfo.h>
-#include <llvm/MC/TargetRegistry.h>
-#include <llvm/Object/ELFObjectFile.h>
-#include <llvm/Object/ObjectFile.h>
-#include <llvm/Support/MemoryBuffer.h>
-#include <llvm/Support/SourceMgr.h>
-#include <llvm/Support/TargetSelect.h>
-#include <llvm/Support/raw_ostream.h>
-#include <llvm/Target/TargetMachine.h>
-
-#include <library/cpp/logger/global/global.h>
-
-#include <util/generic/array_ref.h>
-#include <util/generic/function_ref.h>
-#include <util/generic/hash.h>
 #include <util/generic/maybe.h>
-#include <util/generic/vector.h>
-
-#include <perforator/lib/asm/evaluator.h>
+#include <util/generic/array_ref.h>
 
 namespace NPerforator::NLinguist::NLua::NAsm::NX86 {
 
-TMaybe<i64> DecodeLuaClose(
-    const llvm::Triple& triple, TConstArrayRef<ui8> bytecode);
+TMaybe<i64> DecodeLuaClose(const llvm::Triple& triple, ui64 functionAddress,
+    TConstArrayRef<ui8> bytecode);
 
-TMaybe<i64> DecodeLuaOpenJit(
-    const llvm::Triple& triple, TConstArrayRef<ui8> bytecode);
+TMaybe<i64> DecodeLuaOpenJit(const llvm::Triple& triple, ui64 functionAddress,
+    TConstArrayRef<ui8> bytecode);
 
-TMaybe<i64> DecodeLjDispatchUpdate(
-    const llvm::Triple& triple, TConstArrayRef<ui8> bytecode);
+TMaybe<i64> DecodeLjDispatchUpdate(const llvm::Triple& triple,
+    ui64 functionAddress, TConstArrayRef<ui8> bytecode);
 
 }  // namespace NPerforator::NLinguist::NLua::NAsm::NX86
